b4c6f5540db6397dd9e60186f9538ba07ca1a841
Abu <hugaojun1024@163.com>
2023-03-29 17:39:11 +0800

    data structure two 2

--- src/com/leetcode/acwing/No837_TheNumberOfPointsInTheConnectedBlock.java
+++ src/com/leetcode/acwing/No837_TheNumberOfPointsInTheConnectedBlock.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 /**
  * @author hugaojun Email:nat17185546@163.com
- * @create 2023-03-25-[下午 3:06]-周六
+ * @create 2023-03-28-[上午 8:52]-周二
  */
-public class No836_CombinedSet {
+public class No837_TheNumberOfPointsInTheConnectedBlock {
 
-    private static int n, m;
     private static int N = 100010;
     private static int[] p = new int[N];
+    private static int[] size = new int[N];
+//    private static int n, m;
 
     private static int find(int x) {
-        if (p[x] != x) p[x] = find(p[x]);
+        if (x != p[x]) p[x] = find(p[x]);
         return p[x];
     }
 
@@ -22,21 +23,29 @@ public class No836_CombinedSet {
         int n = in.nextInt();
         int m = in.nextInt();
 
-        //初始化p
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n; i++) {
             p[i] = i;
+            size[i] = 1;
         }
+
         int a, b;
         String opt;
         while (m-- != 0) {
             opt = in.next();
-            a = in.nextInt();
-            b = in.nextInt();
-            if (opt.equals("M")) {
+            if (opt.equals("C")) {
+                a = in.nextInt();
+                b = in.nextInt();
+                if (find(a) == find(b)) continue;
+                size[find(b)] += size[find(a)];
                 p[find(a)] = find(b);
-            } else {
+            } else if (opt.equals("Q1")) {
+                a = in.nextInt();
+                b = in.nextInt();
                 if (find(a) == find(b)) System.out.println("Yes");
                 else System.out.println("No");
+            } else {
+                a = in.nextInt();
+                System.out.println(size[find(a)]);
             }
         }
     }
